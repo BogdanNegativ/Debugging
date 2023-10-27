5487f695402d96a6398201e351d132926a5fc349
Bohdan Liashuk <luashuk2002@gmail.com>
2023-10-27 22:50:53 +0300

    Update division fun

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -12,19 +12,20 @@ class MainActivity : AppCompatActivity() {
         logging()
         division()
     }
-    fun division() {
-        val numerator = 60
-        var denominator = 4
-        repeat(5) {
+    private val numerator = 60
+    private var denominator = 4
+    private fun division() {
+        repeat(4) {
             Log.v(TAG, "${numerator / denominator}")
             denominator--
         }
     }
-    fun logging() {
+    private fun logging() {
         Log.e(TAG, "ERROR: a serious error like an app crash")
         Log.w(TAG, "WARN: warns about the potential for serious errors")
         Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
         Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.d(TAG, "$denominator")
         Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 }
