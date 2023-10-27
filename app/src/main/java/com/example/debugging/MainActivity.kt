f25c94ed3c846b53b4d796b1a2bbeaaaaaf16871
Bohdan Liashuk <luashuk2002@gmail.com>
2023-10-27 22:26:22 +0300

    Add fun logging

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.debugging
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MainActivity"
@@ -8,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun logging() {
+        Log.v(TAG, "Hello, world!")
     }
 }
