938645bcd42899a392014d02e83d537f20233361
Bohdan Liashuk <luashuk2002@gmail.com>
2023-10-27 22:25:44 +0300

    Add const TAG

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.debugging
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
